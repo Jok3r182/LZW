08e132a9663731199f72e65ddd072787167839b2
Jok3r182 <71766496+Jok3r182@users.noreply.github.com>
2021-11-08 19:41:49 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+
+def intToBinary(list):
+    return ''.join(bin(code) for code in list)
+
+
+def stringToBinary(list):
+    return ''.join(bin(ord(char)) for char in list)
+
+
+def compareBits(withCompression, withoutCompression):
+    return len(withoutCompression) - len(withCompression)
+
+
+def LZWCompression(input):
+    dict_size = 256
+
+    # utf-16 chr mes istatydami sk gauname raide, gauname dictionary a:a tarkim, jog paskui pagal
+    # turima value galetume priskirti reiksme
+
+    dictionary = dict((chr(i), i) for i in range(dict_size))
+    string = ""
+    result = []
+    print("Input\tNext\tOutput\tDictionary")
+    for char in input:
+        phrase = string + char
+        # ar fraze yra arba raide
+        if phrase in dictionary:
+            string = phrase
+        else:
+            result.append(dictionary[string])
+            # sukuriame dictioneryje reiksme su size, nes tokia value neegzistuoja
+            dictionary[phrase] = dict_size
+            dict_size += 1
+            print(string + "\t\t" + char + "\t\t" + string + "\t\t" + phrase)
+            string = char
+    if string:
+        result.append(dictionary[string])
+        withCompression = intToBinary(result)
+        withoutCompression = stringToBinary(input)
+        print("\nWithout Compression = " + withoutCompression)
+        print("With Compression = " + withCompression)
+        print("Difference = " + str(compareBits(withCompression, withoutCompression)) + " bits\n")
+
+    return result
+
+
+if __name__ == '__main__':
+    print(LZWCompression("Matas Paulius Dregva"))
